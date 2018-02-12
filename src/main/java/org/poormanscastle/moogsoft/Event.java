28ed425744c316aea3779c7b34471a22f438048c
Georg Federmann <georg.federmann@gmail.com>
2018-02-12 14:37:46 +0100

    Add information about the event fields taken from Moogsoft documentation

--- src/main/java/org/poormanscastle/moogsoft/Event.java
+++ src/main/java/org/poormanscastle/moogsoft/Event.java
@@ -2,18 +2,81 @@ package org.poormanscastle.moogsoft;
 
 import java.util.Date;
 
+/**
+ * An abstraction of an event that will be send to MoogSoft to inform the
+ * event correlation system of events that occurred in the system.
+ * The field are engineered close to the use documentation document as can be found
+ * in the MoogSoft user interface in the Webhook integration section.
+ * The first line of each field's javadoc is quoted directly from the Moogsoft
+ * documentation. The rest of the comment is what we make out of the Moogsoft documentation.
+ */
 public class Event {
 
-    String signature = "my_test_box:application_server:server";
-    String source_id = "198.51.200.3";
+    /**
+     * signature	String	Used to identify the event. Usually source:class:type.
+     * usually a combination of the values of source, class and type,
+     * separated by a colon ":".
+     */
+    String signature = "my_test_box:application_server:server";  // used to identify the id, fqdn:network/hardware/app:storage/latency
+    /**
+     * source	String	Hostname or FQDN of the source machine that generated the event.
+     * we go for:
+     * ServiceHost.example.org, StorageHost.example.org, NAS_Host.example.org, FrontendHost
+     */
+    String source = "Kevins Macbook";   //
+    /**
+     * source_id	String	Unique identifier for the source machine.
+     * we go for configuration management database (CMDB) ID here.
+     */
+    String source_id = "198.51.200.3";  // CMDB_1, CMDB_2, CMDB_3, CMDB_4
+    /**
+     * external_id	String	Unique identifier for the event source.
+     * again: no idea. we go for "dummy"
+     */
     String external_id = "id-1234";
-    String manager = "Kevin";
-    String source = "Kevins Macbook";
-    String eventClass = "server";
-    String agent_location = "London";
-    String type = "Server";
+    /**
+     * agent_location	String	Geographical location of the agent that created the event.
+     * we go for a data center in Frankfurt, floor 2, cage 25:
+     * DC-Fra-02-25
+     */
+    String agent_location = "London";   // DataCenter, Floor, Cage, Fra-02-25
+    /**
+     * severity	Integer	Severity level of the event from 0-5 (clear - critical).
+     * A scala of severity to help moogsoft appraise the weight of the reported event
+     * 0 .. Clear
+     * 1 .. Indeterminate
+     * 2 .. Warning
+     * 3 .. Minor
+     * 4 .. Major
+     * 5 .. Critical
+     */
     Integer severity = 1;
+    /**
+     * type	String	Level of classification for the event. Follows hierarchy class then type.
+     * we go for the following values:
+     * application:performance, application:network, server:storage/network/application, networkdevice:router
+     */
+    String type = "Server";
+    /**
+     * manager	String	General identifier of the event generator or intermediary.
+     * again: no idea. We go for "Testkoch", somebody who is responsible for the given box.
+     */
+    String manager = "Kevin";
+    /**
+     * class	String	Level of classification for the event. Follows hierarchy class then type.
+     * we go for: application, database, infrastructure, storage
+     */
+    String eventClass = "server";
+    /**
+     * description	String	Text description of the event.
+     * A descriptive value that probably helps moogsoft to provide context for the event
+     * and connect it with other events, probably by looking for key words like server names,
+     * application names, stuff like that.
+     */
     String description = "My MacPro is going nuts";
+    /**
+     * The actual time when the event was raised.
+     */
     Long agent_time = new Date().getTime();
 
     public String getJson() {
